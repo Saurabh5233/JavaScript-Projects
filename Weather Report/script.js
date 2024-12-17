a0c960dc6df2804cd313406b85b55dfd9c5ced45
Saurabh5233 <124551411+Saurabh5233@users.noreply.github.com>
2024-12-18 00:36:31 +0530

    Update script.js

--- Weather Report/script.js
+++ Weather Report/script.js
@@ -1,41 +1,34 @@
+let searchBtn = document.getElementById('search-btn');
+let searchArea = document.getElementById('search');
 
+searchBtn.addEventListener('click', async () => {
+  let weatherDetails = document.getElementById("weather-details");
+  weatherDetails.style = "display: flex"
+  let city = searchArea.value;
+  let url = `https://api.weatherapi.com/v1/current.json?key=e884e49500a94e69811163154241309&q=${city};`;
 
-let searchBtn = document.getElementById("submit")
-let searchArea = document.getElementById('city')
+  try {
+    let response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Http error! status: ${response.status}`);
+    }
+    let jsonData = await response.json();
 
-
-
-searchBtn.addEventListener('click',async ()=>{
-    let city = searchArea.value;
-    let url = `http://api.weatherapi.com/v1/current.json?key=e884e49500a94e69811163154241309&q=${city}";`
+    let weatherImage = document.getElementById('img');
+    let condition = document.getElementById('condition');
+    let temp = document.getElementById('temp');
+    let wind = document.getElementById('wind');
     
 
-    try{
-        let response = await fetch(url);
-        if(!response.ok){
-            throw new Error(`Http error! status: ${response.status}`);
-        }
-        let jsonData = await response.json();
-        
-
-        let detail = document.getElementById('details')
-        let temp= document.getElementById('temp');
-        let wind= document.getElementById('wind');
-
-        detail.innerHTML = `<p>Name : ${jsonData['location']["name"]} <br> 
-                                Region: ${jsonData['location']["region"]} <br>   
-                                Country : ${jsonData['location']["country"]} 
-                                ` 
-        temp.innerHTML =   `<p>Celcius : ${jsonData['current']["temp_c"]} <br> 
-                                Farenheit: ${jsonData['current']["temp_f"]} <br>   
-                                Condition : ${jsonData['current']["condition"]["text"]} 
-                                `                    
-        wind.innerHTML =   `<p>Wind Speed(mph) : ${jsonData['current']["wind_mph"]} <br> 
-                                Wind Speed(kph): ${jsonData['current']["wind_kph"]} <br>   
-                                Wind Direction : ${jsonData['current']["wind_dir"]} 
-                                `
-    }catch(error){
-        console.log(error);
-    }
-
+    weatherImage.innerHTML = `<img
+    src="images/${jsonData['current']['condition']['text']}.svg"
+    alt="Weather Condition Icon"
+    class="condition"
+  />`;
+    condition.innerHTML = `${jsonData['current']['humidity']}%`;
+    temp.innerHTML = `${jsonData['current']['temp_c']}°C`;
+    wind.innerHTML = `${jsonData['current']['wind_kph']}Kmph <br> ${jsonData["current"]["wind_dir"]}`;
+  } catch (error) {
+    console.log(error);
+  }
 });
